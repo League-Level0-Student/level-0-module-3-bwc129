84c1ee6e26ab2c053cd07995c476eae00feba471
league <league@192.168.1.200>
2024-05-20 16:58:24 -0700

    h

--- src/_01_else_if/_5_own_adventure/ownAdventure.java
+++ src/_01_else_if/_5_own_adventure/ownAdventure.java
@@ -1,5 +1,171 @@
 package _01_else_if._5_own_adventure;
 
-public class ownAdventure {
+import javax.swing.JOptionPane;
 
+public class ownAdventure {
+public static void main(String[] args) {
+	
+	
+	String r = JOptionPane.showInputDialog("You are in a forest and you want to get out. There are many, many different ways to get out.\n "
+			+ "The first way is to go straight. This way leads to a charging river which has hippopotomuses and piranas.\n"
+			+ " The left leads to a part of the forest which has many different deadly creatures lurking in it.\n "
+			+ "For example there are lepords and snakes. And the right is a safari with lions and rhinos.\n "
+			+ "You will get to saftey after you cross your challenge. Which way are you going to go.");{
+	
+	if (r.equals("straight")) {
+		String h = JOptionPane.showInputDialog("You chose to go straight. You trek through the jungle without any struggle and get to the river.\n"
+				+ " You are tired and want some water. You have 3 options.\n "
+				+ "drink your rations till they are gone, power through with no water, or drink the suspitions river water. What will you do?");
+	}
+	}	
+	
+	if (r.equals("left")) {
+		String m = JOptionPane.showInputDialog("You chose left. The moment you enter the jungle you find dwayne the rock johnson,\n"
+				+ " about to crush john ceeeena but then he sees you and he comes over and boom you are friends with the rock.\n "
+				+ "Do you want him to help you?");
+	}
+	
+	if (r.equals("right")) {
+		String b = JOptionPane.showInputDialog("You chose right. You walk over and you see the lion king.\n"
+				+ " The monkey is walking up to show simba but then he yeets him off the cliff.\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
